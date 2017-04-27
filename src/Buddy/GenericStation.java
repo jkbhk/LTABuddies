416c4b4b5410a6413defbe5174eecd4f7898c5c1
Unknown <marcusheadgotscarm16@gmail.com>
2017-04-27 16:35:24 +0800

    Added GenericStation script

--- src/Buddy/GenericStation.java
+++ src/Buddy/GenericStation.java
@@ -2,10 +2,33 @@ package Buddy;
 
 public class GenericStation
 {
+    protected String name;
+    protected String id; 
 
-    public GenericStation()
+    public GenericStation(String name,String id)
     {
-        
+        this.name = name;
+        this.id = id;
+    }
+    
+    public String GetName()
+    {
+        return this.name;
+    }
+    
+    public void SetName(String name)
+    {
+        this.name = name;
+    }
+    
+    public String GetID()
+    {
+        return this.id;
+    }
+    
+    public void SetID(String id)
+    {
+        this.id = id;
     }
     
     
