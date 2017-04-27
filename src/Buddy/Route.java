2abd51499629a92a396704e27a5b3b45828ef67b
Unknown <marcusheadgotscarm16@gmail.com>
2017-04-27 17:51:22 +0800

    Added Route.java

---
+++ src/Buddy/Route.java
@@ -0,0 +1,27 @@
+
+package Buddy;
+
+import java.util.ArrayList;
+
+
+public class Route {
+    
+    private ArrayList<GenericStation> stations;
+    
+    public Route()
+    {
+        stations = new ArrayList<GenericStation>();
+    }
+    
+    public ArrayList<GenericStation> GetStationsInRoute()
+    {
+        return this.stations;
+    }
+    
+    public void AddStationToRoute(GenericStation genericStation)
+    {
+        this.stations.add(genericStation);
+    }
+    
+    
+}
