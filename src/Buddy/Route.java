38062deffaa3aa868d21fd387aebc99cce276c3e
Unknown <marcusheadgotscarm16@gmail.com>
2017-05-02 16:33:13 +0800

    ?

---
+++ src/Buddy/Route.java
@@ -0,0 +1,42 @@
+
+package Buddy;
+
+import java.util.ArrayList;
+
+
+public class Route {
+    
+    private ArrayList<GenericStation> stations;
+    
+    //Constructor
+    public Route()
+    {
+        stations = new ArrayList<GenericStation>();
+    }
+    
+    public ArrayList<GenericStation> GetStationsInRoute()
+    {
+        return this.stations;
+    }
+    
+    public void AddStationToRoute(GenericStation genericStation)
+    {
+        stations.add(genericStation);
+    }
+    
+    // Returns the total number of stations/nodes in the route
+    public int TotalNumberOfStationsInRoute()
+    {
+        return stations.size();
+    }
+    
+    // Returns the required number of stations/nodes to finish the route
+    public int StationsFromDestination(GenericStation s)
+    {
+        return (stations.size()-1) - stations.indexOf(s);   
+    }
+    
+    
+    
+    
+}
