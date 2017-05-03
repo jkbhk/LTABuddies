e008a9c1270f9f9ab32ff1aa53803e886fd34c65
Unknown <marcusheadgotscarm16@gmail.com>
2017-05-03 18:35:15 +0800

    Slightly modulated  file reader

--- src/Buddy/GenericFileReader.java
+++ src/Buddy/GenericFileReader.java
@@ -7,17 +7,22 @@ import java.util.*;
 
 public class GenericFileReader 
 {
+    private final String busStopPath = "Data/lta-bus_stop_codes.csv";
+    private final String routePath = "Data/lta-sbst_route.csv";;
+    private final String delimiter = ",";
+    private String line = "";
+    private BufferedReader buffer;
+    
+    
+    
+    
     public ArrayList<BusStation> ReadBusStopCode()
     {
-        ArrayList<BusStation> allBusStations = new ArrayList<BusStation>();
-        String filePath = "Data/lta-bus_stop_codes.csv";
-        BufferedReader buffer = null;
-        String line = "";
-        String delimiter = ",";
+        ArrayList<BusStation> allBusStations = new ArrayList<>();
         
         try
         {
-            buffer = new BufferedReader(new FileReader(filePath));
+            buffer = new BufferedReader(new FileReader(busStopPath));
             
             while ((line = buffer.readLine()) != null)
             {
@@ -56,5 +61,50 @@ public class GenericFileReader
         }
         
         return allBusStations;
+    }
+    
+     public ArrayList<Route> ReadRouteCode()
+    {
+        ArrayList<Route> allRoutes = new ArrayList<>();
+        
+        try
+        {
+            buffer = new BufferedReader(new FileReader(routePath));
+            
+            while ((line = buffer.readLine()) != null)
+            {
+                String[] myStrArr = line.split(delimiter);
+                
+                if(myStrArr[0].contains("SVC_NUM"))
+                {
+                    continue;
+                }
+                
+                //TO DO
+                //myStrArr.add(new Route(myStrArr[2], myStrArr[0], myStrArr[1]));
+                
+            }
+        }
+        catch (Exception e)
+        {
+            //e.printStackTrace();
+            System.out.println("Error");
+        }
+        finally
+        {
+            if (buffer != null)
+            {
+                try
+                {
+                    buffer.close();
+                }
+                catch (IOException e)
+                {
+                    e.printStackTrace();
+                }
+            }
+        }
+        
+        return allRoutes;
     }
 }
